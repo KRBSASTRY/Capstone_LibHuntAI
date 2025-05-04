48191f9a6a74d75a8f7f24b2f9f9948f09114ce5
Rama <ramabhadrasastryk@gmail.com>
2025-05-03 22:18:26 -0400

    removed the bug

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,7 +41,7 @@ const sendVerificationCode = async (user) => {
   }
 };
 
-eexports.register = async (req, res) => {
+exports.register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.status(400).json({ message: "All fields are required." });
